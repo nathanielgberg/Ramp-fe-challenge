0c8be0860c23a92775747c3fc8a05ad8d65cb7c7
Nate <nathanielgreenberg5@gmail.com>
2025-02-16 19:37:50 -0800

    first commit

---
+++ ramp-fe-challenge/src/components/Transactions/TransactionPane.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import { InputCheckbox } from "../InputCheckbox"
+import { TransactionPaneComponent } from "./types"
+// bug #7 fix
+import { useCustomFetch } from "../../hooks/useCustomFetch"
+
+export const TransactionPane: TransactionPaneComponent = ({
+  transaction,
+  loading,
+  setTransactionApproval: consumerSetTransactionApproval,
+}) => {
+  const [approved, setApproved] = useState(transaction.approved)
+  // bug #7 fix
+  const { clearCache } = useCustomFetch()
+
+  return (
+    <div className="RampPane">
+      <div className="RampPane--content">
+        <p className="RampText">{transaction.merchant} </p>
+        <b>{moneyFormatter.format(transaction.amount)}</b>
+        <p className="RampText--hushed RampText--s">
+          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+        </p>
+      </div>
+      <InputCheckbox
+        id={transaction.id}
+        checked={approved}
+        disabled={loading}
+        onChange={async (newValue) => {
+          await consumerSetTransactionApproval({
+            transactionId: transaction.id,
+            newValue,
+          })
+          // bug #7 fix
+          await clearCache()
+          setApproved(newValue)
+        }}
+      />
+    </div>
+  )
+}
+
+const moneyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+})
